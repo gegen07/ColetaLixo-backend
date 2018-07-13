de27059a00efd3c8fd6e870dc47197851ef7cc1d
Germano Barcelos <germanobarcelos@yahoo.com.br>
2018-07-12 22:23:41 -0300

    Added Communication between Station and Company
    One Requirement specified in README concluded

--- collectDumpAPI/app/Http/Controllers/StationSellController.php
+++ collectDumpAPI/app/Http/Controllers/StationSellController.php
@@ -6,67 +6,83 @@ use Illuminate\Support\Facades\DB;
 use App\Models\StationSell;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
- 
+use App\Transformers\StationSellTransformer;
+use Dingo\Api\Routing\Helpers;
+use Illuminate\Support\Facades\Validator;
+  
 class StationSellController extends Controller{
+
+    use Helpers;
 
 	public function create(Request $request){
 
-        $this->validate($request, [
-            'price'     => 'required',
-            'quantity'  => 'required',
-            'type_id'   => 'required',
-        ]);
- 
-    	$stationSell = StationSell::create($request->all());
- 
-    	return response()->json(['data' => $stationSell]);
+		$rules = [
+            'price' => ['required'],
+            'quantity' => ['required'],
+            'type_id' => ['required'],
+            'idStation' => ['required']
+		];
+		$validator = Validator::make($request->all(), $rules);
+
+		if (!$validator->fails()) {
+			$stationSell = StationSell::create($request->all());
+			return $this->response->item($stationSell, new StationSellTransformer);
+		} else {
+			throw new \Dingo\Api\Exception\StoreResourceFailedException('Could not create new sell of station.', $validator->errors());
+		} 
  
 	}
  
 	public function update(Request $request, $id){
 
-        $this->validate($request, [
-            'price'     => 'required',
-            'quantity'  => 'required',
-            'type_id'   => 'required',
-        ]);
+        try {
+            $stationSell = StationSell::findOrFail($id);
 
-    	$stationSell = StationSell::find($id);
-        $stationSell->price = $request->input('price');
-        $stationSell->quantity = $request->input('quantity');
-        $stationSell->type_id = $request->input('type_id');
-        $stationSell->station_id = $request->input('station_id');
-    	$stationSell->save();
- 
-    	return response()->json(['data' => $stationSell]);
+            if ($request->has('price')) {
+                $stationSell->price = $request->input('price');
+            }
+            if($request->has('quantity')) {
+                $stationSell->quantity = $request->input('quantity');
+            }
+            if($request->has('type_id')) {
+                $stationSell->type_id = $request->input('type_id');
+            }
+            if($request->has('idStation')){
+                $stationSell->station_id = $request->input('idStation');
+            }
+            $stationSell->save();
+            return $this->response->item($stationSell, new StationSellTransformer)->setStatusCode(200);
+        } catch (\Exception $e) {
+            throw new \Symfony\Component\HttpKernel\Exception\NotFoundHttpException('Could not found sell of station');
+        }
 	}  
 
 	public function delete($id){
 
-        try {
-            $stationSell = StationSell::findOrFail($id);
-            $stationSell->delete();
-        } catch(\Exception $e) {
-                $stationSell = null;
-                $statusCode = 404;
-        }
- 
-    	return response()->json([
-            "data" => $stationSell,
-            "status" => $stationSell ? "success" : "Not found."
-        ], $statusCode ?? 200
-        );
+        $stationSell = StationSell::destroy($id);
+		if($stationSell) {
+			return response(
+				[
+					'status' => $stationSell ? "success" : "Not found.",
+				], $statusCode ?? 201
+			);
+		} else {
+			throw new \Symfony\Component\HttpKernel\Exception\NotFoundHttpException('Could not found sell of station');
+		}
 	}
 
 	public function index(){
- 
-    	return response()->json(['data'=> StationSell::with('typename')->get()]);
- 
+        $stationSell = StationSell::paginate(12);
+		return $this->response->paginator($stationSell, new StationSellTransformer);  
     }
 
     public function show($id) {
-        $stationSells = StationSell::with('typename')->findOrFail($id);
-        return response()->json($stationSells);        
+        try {
+            $stationSell = StationSell::with('typename')->findOrFail($id);
+            return $this->response->item($stationSell, new StationSellTransformer)->setStatusCode(200);
+        } catch (\Exception $e) {
+            throw new \Symfony\Component\HttpKernel\Exception\NotFoundHttpException('Could not found sell of station');
+        }       
     }
 
     public function searchTypeName($name) {
@@ -76,11 +92,9 @@ class StationSellController extends Controller{
 
         $stationSells = StationSell::with('typename')
                             ->where('type_id', '=', $type->id)
-                            ->get(); 
+                            ->paginate(12); 
         
-        return response()->json([
-            'data'=> $stationSells
-            ]);        
+        return $this->response->paginator($stationSells, new StationSellTransformer);     
     }
 }
 ?>
